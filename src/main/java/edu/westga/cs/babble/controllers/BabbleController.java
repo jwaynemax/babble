db42aaf239e9432f44512c330e7422ef8b2c9e21
jm00724 <jm00724@csvlab-vm18.uwg.westga.edu>
2023-08-22 13:03:59 -0400

    Initialize tileBag and tileRack and populate tile listview

--- src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
@@ -1,5 +1,10 @@
 package edu.westga.cs.babble.controllers;
 
+import edu.westga.cs.babble.model.EmptyTileBagException;
+import edu.westga.cs.babble.model.Tile;
+import edu.westga.cs.babble.model.TileBag;
+import edu.westga.cs.babble.model.TileGroup;
+import edu.westga.cs.babble.model.TileRack;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -9,24 +14,36 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
 
 public class BabbleController {
-	
-	 @FXML
-	    private ListView<String> listView;
 
-	    @FXML
-	    private Button addButton;
+	@FXML
+	private ListView<Character> listView;
+	private ObservableList<Character> item;
+	private Button addButton;
 
-	    private ObservableList<String> items;
+	private TileBag tileBag = new TileBag();
+	private TileRack tileRack = new TileRack();
 
-	    public void initialize() {
-	        items = FXCollections.observableArrayList();
-	        listView.setItems(items);
-	        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-	    }
+	public void initialize() throws EmptyTileBagException {
 
-	    @FXML
-	    private void handleAddButtonAction(ActionEvent event) {
-	        items.add("New Item");
-	    }
+		while (tileRack.getNumberOfTilesNeeded() != 0) {
+			tileRack.append(tileBag.drawTile());
+		}
+
+		item = FXCollections.observableArrayList();
+		int i = 0;
+		while (i != 7) {
+			item.add(tileRack.tiles().get(i).getLetter());
+			i++;
+		}
+
+		listView.setItems(item);
+
+		listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+	}
+
+	@FXML
+	private void handleAddButtonAction(ActionEvent event) {
+		item.add('c');
+	}
 
 }
