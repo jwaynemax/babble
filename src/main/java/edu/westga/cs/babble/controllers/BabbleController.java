c76af589a347833a42c4fc4b32c014233c3f27a3
jm00724 <jm00724@csvlab-vm19.uwg.westga.edu>
2023-08-22 22:52:10 -0400

    added logic to remove tiles when clicked

--- src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleController.java
@@ -13,17 +13,21 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.ListCell;
+import javafx.util.Callback;
+
+
 
 public class BabbleController {
 
 	@FXML
-	private ListView<Character> tileListView;
+	private ListView<Tile> tileListView;
 	
 	@FXML
-	private ListView<Character> wordListView;
+	private ListView<Tile> wordListView;
 	
-	private ObservableList<Character> item;
-	private ObservableList<Character> clickedItem;
+	private ObservableList<Tile> item;
+	private ObservableList<Tile> clickedItem;
 	private Button addButton;
 
 	private TileBag tileBag = new TileBag();
@@ -35,12 +39,19 @@ public class BabbleController {
 	public void initialize() throws EmptyTileBagException {	
 		item = FXCollections.observableArrayList();
 		tileListView.setItems(item);
+		
+		tileListView.setCellFactory(new Callback<ListView<Tile>, ListCell<Tile>>() {
+            @Override
+            public ListCell<Tile> call(ListView<Tile> param) {
+                return new TileListCell();
+            }
+        });
 
 		
 		while (tileRack.getNumberOfTilesNeeded() != 0) {
 			tile = tileBag.drawTile();
 			tileRack.append(tile);
-			item.add(tile.getLetter());
+			item.add(tile);
 		}
 
         
@@ -49,23 +60,46 @@ public class BabbleController {
 
         
 		tileListView.setOnMouseClicked(event -> {
-            Character clickedTile = tileListView.getSelectionModel().getSelectedItem();
-            tile = new Tile(clickedTile);
-            wordRack.append(tile);
+            Tile clickedTile = tileListView.getSelectionModel().getSelectedItem();
+            wordRack.append(clickedTile);
             System.out.println(wordRack.getHand());
             clickedItem.add(clickedTile);
             //remove tile from tileRack
+            wordListView.setCellFactory(new Callback<ListView<Tile>, ListCell<Tile>>() {
+                @Override
+                public ListCell<Tile> call(ListView<Tile> param) {
+                    return new TileListCell();
+                }
+            });
             
-            System.out.println(tileRack.tiles().indexOf(0));
+            try {
+				tileRack.remove(clickedTile);
+				item.remove(clickedTile);
+			} catch (TileNotInGroupException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         });
 		
 		
 
 	}
+	
+	private class TileListCell extends ListCell<Tile> {
+        @Override
+        protected void updateItem(Tile item, boolean empty) {
+            super.updateItem(item, empty);
+            if (empty || item == null) {
+                setText(null);
+            } else {
+                setText(Character.toString(item.getLetter())); // Display the string representation of the Word object
+            }
+        }
+    }
 
 	@FXML
 	private void handleAddButtonAction(ActionEvent event) {
-		item.add('c');
+		item.add(tile);
 	}
 
 }
