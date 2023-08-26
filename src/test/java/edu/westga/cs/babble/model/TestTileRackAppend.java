1e8564ae6e3ca391b22e5c3a43f8bcafea8f59f9
jm00724 <jm00724@csvlab-vm11.uwg.westga.edu>
2023-08-26 17:24:04 -0400

    TestTileRackAppend created and tests validated

---
+++ src/test/java/edu/westga/cs/babble/model/TestTileRackAppend.java
@@ -0,0 +1,35 @@
+package edu.westga.cs.babble.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestTileRackAppend {
+	
+	private TileRack tileRack = new TileRack();
+
+	@Test
+	void shouldNotAppendToFullRack() {
+		Tile tile1 = new Tile('c');
+		Tile tile2 = new Tile('e');
+		Tile tile3 = new Tile('g');
+		Tile tile4 = new Tile('t');
+		Tile tile5 = new Tile('y');
+		Tile tile6 = new Tile('z');
+		Tile tile7 = new Tile('x');
+		Tile tile8 = new Tile('k');
+		
+		this.tileRack.append(tile1);
+		this.tileRack.append(tile2);
+		this.tileRack.append(tile3);
+		this.tileRack.append(tile4);
+		this.tileRack.append(tile5);
+		this.tileRack.append(tile6);
+		this.tileRack.append(tile7);
+		
+		assertThrows(TileRackFullException.class, () -> {
+			this.tileRack.append(tile8);
+		});
+	}
+
+}
